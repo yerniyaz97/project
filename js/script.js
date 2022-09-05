1cb2d79318d6e70f86a855a13dd59fed5ecf96c0
Yerniyaz <example@gmail.com>
2022-09-05 22:37:48 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+
+alert(5)
+[].push('a');
